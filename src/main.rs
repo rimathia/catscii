7c8feef851b28a932c5fd3c5267f7dfed3159ceb
rimathia <mathias.ritzmann@gmail.com>
2023-07-31 14:34:27 +0000

    separate function for getting the url

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,32 @@
+use color_eyre::owo_colors::OwoColorize;
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct CatImage {
+    id: String,
+    url: String,
+    width: usize,
+    height: usize,
+}
+
+async fn get_cat_image_url() -> color_eyre::Result<String> {
+    let api_url = "https://api.thecatapi.com/v1/images/search";
+    let res = reqwest::get(api_url).await?;
+    if !res.status().is_success() {
+        return Err(color_eyre::eyre::eyre!(
+            "api returned status {}",
+            res.status()
+        ));
+    }
+    let images: Vec<CatImage> = res.json().await?;
+    let Some(image) = images.into_iter().next() else {
+        return Err(color_eyre::eyre::eyre!("there is no image"));
+    };
+    Ok(image.url)
+}
+
 #[tokio::main]
 async fn main() {
-    let res = reqwest::get("https://api.thecatapi.com/v1/images/search")
-        .await
-        .unwrap();
-    println!("Status: {}", res.status());
-    let body = res.text().await.unwrap();
-    println!("Body: {}", body);
+    let image_url = get_cat_image_url().await;
+    println!("url: {}", image_url.unwrap());
 }
